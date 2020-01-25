ca37603e01508690f1773eb96bb18aa880ea3eef
Brad <bbrown4898@gmail.com>
2020-01-25 12:58:06 -0500

    Add files via upload

--- install.py
+++ install.py
@@ -1,13 +1,15 @@
 #!/usr/bin/evn python3
 
+
 import os
+
+GOO_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
 
 print("Installing Requirements...")
 os.system("pip3 install pymetasploit3 netifaces sqlite3 json")
-print("Creating directory /opt/venomgoo ...")
-os.system("mkdir -p /opt/venomgoo")
 print("Copying Files ...")
-os.system("cp ./* /opt/venomgoo/")
+os.system("cp -r %s /opt" %GOO_PATH)
 print("Linking script ...")
 os.system("ln -s /opt/venomgoo/VenomGoo.py /usr/bin/venomgoo")
 print("Done.")
